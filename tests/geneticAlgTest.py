73ac26feda65bb12c4693f7b42849472331c31eb
washt <washt23@gmail.com>
2015-07-16 17:19:21 -0400

    stubbed out the rest of the genetic algorithm tests...for now

--- tests/geneticAlgTest.py
+++ tests/geneticAlgTest.py
@@ -1,1 +1,74 @@
 import unittest
+
+class genomeTest(unittest.TestCase):
+
+	def testFitness(self):
+		self.Fail("Not implemented")
+
+	def testgetWeights(self):
+		self.Fail("Not implemented")	
+
+class genAlgTest(unittest.TestCase):
+
+	def testpopSize(self):
+		self.Fail("Not implemented")
+	
+	def testlenChromosome(self):
+		self.Fail("Not implemented")
+	
+	def testtotalFit(self):
+		self.Fail("Not implemented")
+
+	def testbestFit(self):
+		self.Fail("Not implemented")	
+
+	def testaveFit(self):
+		self.Fail("Not implemented")
+
+	def testworstFit(self):
+		self.Fail("Not implemented")
+
+	def testbestGenome(self):
+		self.Fail("Not implemented")
+
+	def testmutationRate(self):
+		self.Fail("Not implemented")
+
+	def testcrossRate(self):
+		self.Fail("Not implemented")
+
+	def testgeneration(self):
+		self.Fail("Not implemented")
+
+	def testcrossover(self,ma,fa,bab1,bab2):
+		self.Fail("Not implemented")
+
+	def testmutate(self,chromosome):
+		self.Fail("Not implemented")
+
+	def testgetRandomchromo(self):
+		self.Fail("Not implemented")
+
+	def testgetBestrandom(self,best,numclones,pop):
+		self.Fail("Not implemented")
+
+	def testaverage(self):
+		self.Fail("Not implemented")
+
+	def testreset(self):
+		self.Fail("Not implemented")
+
+	def testgen(self,popsize,mut,cross,numweights):
+		self.Fail("Not implemented")
+
+	def testEpoch(self):
+		self.Fail("Not implemented")
+
+	def testgetChromosomes(self):
+		self.Fail("Not implemented")
+
+	def testgetAveragefitness(self):
+		self.Fail("Not implemented")
+
+	def testgetBestFitness(self):
+		self.Fail("Not implemented")
