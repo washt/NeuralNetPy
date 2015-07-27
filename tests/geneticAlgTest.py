840b5f8907fcd1cd72c17a4b492add53cb5dbbc3
washt <washt23@gmail.com>
2015-07-27 13:59:08 -0400

    genome tests working and passing, added init files, fixed missing colon in genAlg

--- tests/geneticAlgTest.py
+++ tests/geneticAlgTest.py
@@ -1,79 +1,86 @@
-from geneticAlg import *
+import sys
 import unittest
+sys.path.append("..")
+from geneticAlg import *
 
 class genomeTest(unittest.TestCase):
 
 	def testFitness(self):
-		genome = genome()
+		
+		gen = genome()
 
-		self.assertEqual(genome.fitness,0.0)
+		self.assertEqual(gen.fitness,0.0)
 
 	def testgetWeights(self):
-		genome = genome()
+		
+		gen = genome()
 
-		self.assertEqual(len(genome.weights),0)
+		self.assertEqual(len(gen.weights),0)
 
 class genAlgTest(unittest.TestCase):
 
 	def testpopSize(self):
-		self.Fail("Not implemented")
+		self.fail("Not implemented")
 	
 	def testlenChromosome(self):
-		self.Fail("Not implemented")
+		self.fail("Not implemented")
 	
 	def testtotalFit(self):
-		self.Fail("Not implemented")
+		self.fail("Not implemented")
 
 	def testbestFit(self):
-		self.Fail("Not implemented")	
+		self.fail("Not implemented")	
 
 	def testaveFit(self):
-		self.Fail("Not implemented")
+		self.fail("Not implemented")
 
 	def testworstFit(self):
-		self.Fail("Not implemented")
+		self.fail("Not implemented")
 
 	def testbestGenome(self):
-		self.Fail("Not implemented")
+		self.fail("Not implemented")
 
 	def testmutationRate(self):
-		self.Fail("Not implemented")
+		self.fail("Not implemented")
 
 	def testcrossRate(self):
-		self.Fail("Not implemented")
+		self.fail("Not implemented")
 
 	def testgeneration(self):
-		self.Fail("Not implemented")
+		self.fail("Not implemented")
 
 	def testcrossover(self,ma,fa,bab1,bab2):
-		self.Fail("Not implemented")
+		self.fail("Not implemented")
 
 	def testmutate(self,chromosome):
-		self.Fail("Not implemented")
+		self.fail("Not implemented")
 
 	def testgetRandomchromo(self):
-		self.Fail("Not implemented")
+		self.fail("Not implemented")
 
 	def testgetBestrandom(self,best,numclones,pop):
-		self.Fail("Not implemented")
+		self.fail("Not implemented")
 
 	def testaverage(self):
-		self.Fail("Not implemented")
+		self.fail("Not implemented")
 
 	def testreset(self):
-		self.Fail("Not implemented")
+		self.fail("Not implemented")
 
 	def testgen(self,popsize,mut,cross,numweights):
-		self.Fail("Not implemented")
+		self.fail("Not implemented")
 
 	def testEpoch(self):
-		self.Fail("Not implemented")
+		self.fail("Not implemented")
 
 	def testgetChromosomes(self):
-		self.Fail("Not implemented")
+		self.fail("Not implemented")
 
 	def testgetAveragefitness(self):
-		self.Fail("Not implemented")
+		self.fail("Not implemented")
 
 	def testgetBestFitness(self):
-		self.Fail("Not implemented")
+		self.fail("Not implemented")
+
+if __name__ == '__main__':
+	unittest.main()
