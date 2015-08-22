4bba4e94467a8fe5969a22ad5a5d2eb0a8ee3c0b
washt <washt23@gmail.com>
2015-08-22 14:32:29 -0400

    initial controller class for NEAT g.e.

---
+++ neat.py
@@ -0,0 +1,31 @@
+from NeuralNet import *
+import random as ran
+
+class neat(object):
+	"""implementation of the Neural Evolution for Augmented Topologies (NEAT)
+		algoritm. [SOURCE]"""
+
+	def __init__(self):
+		
+		self.generation 	= []
+		self.genotype 		= []
+		self.phenotype 		= []
+		self.global_inno_count = 0
+		self.population = []
+		self.populationsize = 100
+
+		for i in range(self.populationsize):
+			#create an initial population with zero hidden nodes
+			# with 2 inputs and 1 ouput, and a random bias
+			self.population.append(nnet(2,1,0,0,ran.uniform(-1.0,1.0)))
+
+	def generation():
+
+		raise NotImplementedError
+
+	def crossover():
+
+		raise NotImplementedError
+
+lol = neat()
+		
